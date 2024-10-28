617993d46868270b7dec0aed0345f4d86b5eb3ca
Julien Lucadello <lucadello.julien@hotmail.fr>
2024-10-28 13:28:54 +0100

    Passage par valeur ou réference

--- 03.les_fonctions/03.Passage_par_valeur_ou_référence/src/App.java
+++ 03.les_fonctions/03.Passage_par_valeur_ou_référence/src/App.java
@@ -1,5 +1,14 @@
+import java.util.ArrayList;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        
+    public static void main(String[] args) {
+        var list  = new ArrayList<String>(); 
+        list.add("value1");
+        modify(list);
+        System.out.println("Valeur de list après modification : " + list);
+    }
+
+    private static void modify(ArrayList<String> list) {
+        list.add("value2"); 
     }
 }
