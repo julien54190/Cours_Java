24865fea34deb5d33a928090efc7f41873a5f360
Julien Lucadello <lucadello.julien@hotmail.fr>
2024-10-28 14:28:41 +0100

    les classes et objets

---
+++ 04.programmation_oriantée_objet/01.les_classes_et_objets/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Fruit fruit = new Fruit();
+        var fruit2 = new Fruit();
+
+        Fruit fruit3 = null;
+        fruit3 = new Fruit();
+    }
+}
